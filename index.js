35452e4adcdf996bf85566896a6901b66132e343
shezhiqiang <shezhiqiang@xiaomei.com>
2016-02-03 16:20:08 +0800

    monitor port 80

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var http = require('http');
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
 var fs = require('fs');
-var serverPort = process.env.PORT || 3000;
+var serverPort = process.env.PORT || 80;
 
 // swaggerRouter configuration
 var options = {
